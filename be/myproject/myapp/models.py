99cb78cc097e31d8afdbe24e93bc02e520d65ae6
Lucas HU <986117324@qq.com>
2024-07-01 22:56:29 +1000

    chore: Refactor models.py

--- be/myproject/myapp/models.py
+++ be/myproject/myapp/models.py
@@ -5,7 +5,6 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from rest_framework.authtoken.models import Token
 
 
-# Users 表
 class User(models.Model):
     UserID = models.AutoField(primary_key=True)
     FirstName = models.CharField(max_length=50)
@@ -26,7 +25,7 @@ class Project(models.Model):
     ProjectDescription = models.TextField()
     ProjectOwner = models.CharField(max_length=255)
     CreatedBy = models.ForeignKey(User, related_name='created_projects', on_delete=models.CASCADE)
-    MaxNumOfGroup = models.IntegerField(default=0)  # 添加新字段 并且 设置默认值
+    MaxNumOfGroup = models.IntegerField(default=1)  # 添加新字段 并且 设置默认值
 
     def __str__(self):
         return str(self.ProjectID)
@@ -78,28 +77,6 @@ class GroupProjectsLink(models.Model):
         return str(self.GroupProjectsLinkID)
 
 
-# # Student Sign up ?
-# class UserProfile(models.Model):
-#     username = models.CharField(max_length=255)
-#     email = models.CharField(max_length=255)
-#     password = models.CharField(max_length=255)
-
-#     def __str__(self):
-#         return self.username
-
-# # Project Creation and Updating
-# class Project(models.Model):
-#     title = models.CharField(max_length=255)
-#     description = models.TextField()
-#     required_skills = models.CharField(max_length=255)
-#     timeline = models.CharField(max_length=255)
-#     related_course = models.CharField(max_length=255)
-#     specific_student_criteria = models.CharField(max_length=255)
-#     created_by = models.ForeignKey(UserProfile, on_delete=models.CASCADE) # When the UserProfile record is deleted, delete all Project records associated with that
-
-#     def __str__(self):
-#         return self.title
-# 兴趣领域表
 class Area(models.Model):
     AreaID = models.AutoField(primary_key=True)
     AreaName = models.CharField(max_length=255)
@@ -108,7 +85,7 @@ class Area(models.Model):
         return self.AreaName
 
 
-# 学生兴趣配对表
+
 class StudentArea(models.Model):
     StudentAreaID = models.AutoField(primary_key=True)
     Area = models.ForeignKey(Area, on_delete=models.CASCADE)
@@ -118,17 +95,17 @@ class StudentArea(models.Model):
         return f'{self.User} - {self.Area}'
 
 
-# 技能表
+
 class Skill(models.Model):
     SkillID = models.AutoField(primary_key=True)
     SkillName = models.CharField(max_length=255)
-    Area = models.ForeignKey(Area, on_delete=models.CASCADE, default=1)  # 设置默认值
+    Area = models.ForeignKey(Area, on_delete=models.CASCADE)  # 设置默认值
 
     def __str__(self):
         return self.SkillName
 
 
-# 技能与项目关联表
+
 class SkillProject(models.Model):
     SkillProjectID = models.AutoField(primary_key=True)
     Skill = models.ForeignKey(Skill, on_delete=models.CASCADE)
