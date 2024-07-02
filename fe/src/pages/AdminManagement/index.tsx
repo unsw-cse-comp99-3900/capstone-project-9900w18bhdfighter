58ea4e5c45074894527b844a5937e56f59988c0f
Lucas HU <986117324@qq.com>
2024-07-02 16:38:42 +1000

    chore: Refactor AdminManagement page to use TextFilter component for column search

--- fe/src/pages/AdminManagement/index.tsx
+++ fe/src/pages/AdminManagement/index.tsx
@@ -1,13 +1,14 @@
 import { useRef, useState } from 'react'
 import { SearchOutlined } from '@ant-design/icons'
 import type { InputRef, TableColumnsType, TableColumnType } from 'antd'
-import { Button, Divider, Flex, Input, Modal, Space, Table } from 'antd'
+import { Button, Divider, Flex, Modal, Table } from 'antd'
 import type { FilterDropdownProps } from 'antd/es/table/interface'
 import styled from 'styled-components'
 import { role, roleNames, roleNamesEnum } from '../../constant/role'
 import { nanoid } from 'nanoid'
 import { useAuthContext } from '../../context/AuthContext'
 import ModalProfileEdit from '../../components/ModalProfileEdit'
+import TextFilter from './components/TextFilter'
 
 interface DataType {
   key: string
@@ -58,65 +59,14 @@ const AdminManagement = () => {
   const getColumnSearchProps = (
     dataIndex: DataIndex
   ): TableColumnType<DataType> => ({
-    filterDropdown: ({
-      setSelectedKeys,
-      selectedKeys,
-      confirm,
-      clearFilters,
-      close,
-    }) => (
-      <Flex
-        vertical
-        style={{ padding: 8 }}
-        onKeyDown={(e) => e.stopPropagation()}
-      >
-        <Input
-          ref={searchInput}
-          placeholder={`Search ${dataIndex}`}
-          value={selectedKeys[0]}
-          onChange={(e) =>
-            setSelectedKeys(e.target.value ? [e.target.value] : [])
-          }
-          onPressEnter={() => handleSearch(confirm)}
-          style={{ marginBottom: 8, display: 'block' }}
-        />
-        <Space>
-          <Button
-            type="primary"
-            onClick={() => handleSearch(confirm)}
-            icon={<SearchOutlined />}
-            size="small"
-            style={{ width: 90 }}
-          >
-            Search
-          </Button>
-          <Button
-            onClick={() => clearFilters && handleReset(clearFilters)}
-            size="small"
-            style={{ width: 90 }}
-          >
-            Reset
-          </Button>
-          <Button
-            type="link"
-            size="small"
-            onClick={() => {
-              confirm({ closeDropdown: false })
-            }}
-          >
-            Filter
-          </Button>
-          <Button
-            type="link"
-            size="small"
-            onClick={() => {
-              close()
-            }}
-          >
-            close
-          </Button>
-        </Space>
-      </Flex>
+    filterDropdown: (props) => (
+      <TextFilter
+        dataIndex={dataIndex}
+        handleReset={handleReset}
+        handleSearch={handleSearch}
+        searchInput={searchInput}
+        {...props}
+      />
     ),
     filterIcon: (filtered: boolean) => (
       <SearchOutlined style={{ color: filtered ? '#1677ff' : undefined }} />
